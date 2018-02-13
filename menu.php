3432fc582e62678f7bbb3d61b5a562828360d7d6
Khadim96 <35935576+Khadim96@users.noreply.github.com>
2018-02-13 12:08:48 +0000

    First Test

---
+++ menu.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="<?php echo $base_url; ?>public/css/bootstrap-cerulean.min.css"/>
+</head>
+<body>
+
+<div class="navbar navbar-inverse">
+    <ul class="nav navbar-nav">
+        <li><a href="?page=user/add"> Add User</a> </li>
+        <li><a href="?page=user/list">Liste Users</a> </li>
+        <li><a href="?page=role/add">Add Role</a> </li>
+        <li><a href="?page=role/list">Liste des Roles</a> </li>
+        <li><a href="?page=user_role/list">Liste Users_Roles</a> </li>
+    </ul>
+</div>
+
+</body>
+</html>
